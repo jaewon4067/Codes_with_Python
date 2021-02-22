6f541647fabce7af387ecf327601115ea97cb374
Jaewon Lee <58613244+jaewon4067@users.noreply.github.com>
2021-02-22 19:15:42 +0000

    Update Maximum Profit.py

--- Algorithms Examples/Maximum Profit.py
+++ Algorithms Examples/Maximum Profit.py
@@ -142,6 +142,10 @@ def sublist_max(profits):
         max_profit_so_far = max(max_profit_so_far, max_check)
     
     return max_profit_so_far
+
+#Test
+print(sublist_max([7, -3, 4, -8]))
+print(sublist_max([-2, -3, 4, -1, -2, 1, 5, -3, -1]))
     
 """
 
