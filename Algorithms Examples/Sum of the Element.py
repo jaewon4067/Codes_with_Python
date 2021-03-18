b6cc0ca311c666d112fe08cb60f29c071004e3dc
Jaewon Lee <58613244+jaewon4067@users.noreply.github.com>
2021-03-18 11:26:52 +0000

    Update Sum of the Element.py

--- Algorithms Examples/Sum of the Element.py
+++ Algorithms Examples/Sum of the Element.py
@@ -47,7 +47,7 @@ def sum_in_list(search_sum, sorted_list):
     # Cannot find any combinations, so return 'False'
     return False
     
-# 테스트
+# Test
 print(sum_in_list(15, [1, 2, 5, 6, 7, 9, 11]))
 print(sum_in_list(15, [1, 2, 5, 7, 9, 11]))
 
